4ecd8913a0b635bf0db57d45949a5b47b1aabcd1
Dina <dinagiacobbi@gmail.com>
2024-01-14 10:26:03 -0900

    Adding Modal visibility handler

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -7,6 +7,12 @@ import Modal from './Modal'
 const PostsList = () => {
   const [enteredData, setEnteredData] = useState('')
   const [enteredAuthor, setEnteredAuthor] = useState('')
+  const [modalIsVisible, setmodalIsVisible] = useState(true)
+
+  function hideModalHandler() {
+    setmodalIsVisible(false)
+    
+  }
 
   function dataChangeHandler(event) {
     setEnteredData(event.target.value)
@@ -18,12 +24,16 @@ const PostsList = () => {
 
   return (
     <>
-      <Modal>
-        <NewPost
-          onDataChange={dataChangeHandler}
-          onAuthorChange={authorChangeHandler}
-        />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onDataChange={dataChangeHandler}
+            onAuthorChange={authorChangeHandler}
+            
+          />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredData} />
         <Post author="Sam" body="Moose like to eat pumpkins" />
